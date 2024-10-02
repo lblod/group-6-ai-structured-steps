2065b73fd4a0192f297222ad9aeb23234cebeff5
jonasdv97 <jonasdevos5@gmail.com>
2024-10-02 11:18:08 +0200

    webservice

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import request
+import logging
+from sparql import query_product_with_uri
+
+# setup logging
+logging.basicConfig(level=logging.INFO, format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M")
+logging.info('logger initialised')
+
+@app.route("/get_structured_steps", methods=["POST"])
+def ingest(uri):
+
+    logging.info(f'start process of finding structured steps for product {uri}')
+    product = query_product_with_uri(uri)[0] # take one
+
+    # todo: make structured
+
+
+    return "RESULT"
